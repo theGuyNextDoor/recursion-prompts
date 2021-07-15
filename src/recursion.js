c2f1fef870c9d21dd9662f119582015bf04b81fa
Raeye Jordan <raeyejordaan@yahoo.com>
2021-07-14 23:01:44 -0700

    Implemented sum

--- src/recursion.js
+++ src/recursion.js
@@ -6,19 +6,27 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
+
+/*
+  Recursion zoom video code: QRe.w?2L
+*/
+
 var factorial = function(n) {
-    if (n < 0) {
-      return null
-    }
-    if (n === 0) {
+    if (n < 0)
+      return null;
+    if (n === 0)
       return 1;
-    }
     return factorial(n-1) * n;
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array[0] + sum(array.slice(1));
+
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
